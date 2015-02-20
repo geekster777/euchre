35cc3540a2924606b392604326578cd03e61ee00
Nick Hilton <hilton@csh.rit.edu>
2015-02-20 23:18:13 +0000

    Added some simple message handling methods for the game.

--- euchre.py
+++ euchre.py
@@ -1,15 +1,23 @@
-import threading, redis
+import threading, redis, json
 from sys import stdin, stdout
 
 Redis = redis.StrictRedis(host='localhost', port=6379, db=0)
 sessions = Redis.pubsub()
 games = {}
+username = 'user'
 thread = None
+
+def sendMessage(messageObj):
+    print(json.dumps(messageObj))
+    stdout.flush()
 
 def handleMessage(message):
     args = message.split()
-    
-    if args[0] == 'join':
+
+    if args[0] == 'login':
+        global username
+        username = args[1]
+    elif args[0] == 'join':
         stdout.flush()
         joinSession(args[1])
     elif args[0] == 'message':
@@ -20,6 +28,7 @@ def joinSession(sessionName):
     # Start subscribing to session messages
     sessions.subscribe(**{sessionName: handleSession})
     games[sessionName] = {}
+    Redis.public(sessionName, json.dumps({type:'join',username:username}))
     
     if thread == None:
         thread = sessions.run_in_thread(sleep_time=0.001)
@@ -30,8 +39,31 @@ def handleSession(message):
         return
     if message['type'] != 'message':
         return
-    print(message['data'])
-    stdout.flush()
+
+    handleGame(message['data'], message['channel'])
+
+def handleGame(message, session):
+    data = json.loads(message)
+    game = games[session]
+
+    if data.type == 'join':
+        playerId = len(game.players)
+        game.players[playerId] = data.username
+    if data.type == 'player':
+        game.players[data.id] = data.username
+    if data.type == 'dealer':
+        game.dealing = true
+    if data.type == 'trump':
+        game.trump = data.suit
+    if data.type == 'dealtCard':
+        game.hand.append(data.card)
+    if data.type == 'playCard':
+        game.gameCards.append(data.card)
+        sendMessage({type:'playCard', user:data.username, card:data.card})
+        
+        if len(game.gameCards) == 4:
+            winner = calcWinner(game.gameCards)
+            sendMessage({type:'winner', winner:game.players[winner]})
 
 while True:
     line = stdin.readline().strip()
