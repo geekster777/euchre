871564a1fe70254b6637e69fd1f21e9b6b89ded4
Nick Hilton <hilton@csh.rit.edu>
2015-02-22 21:38:13 +0000

    Added more commands that the Redis messages will listen for. Also, added some comments and made the previous commands more robust.

--- euchre.py
+++ euchre.py
@@ -27,14 +27,20 @@ def joinSession(sessionName):
     global thread
     # Start subscribing to session messages
     sessions.subscribe(**{sessionName: handleSession})
+
+    # initialize the game
     games[sessionName] = {}
-    Redis.public(sessionName, json.dumps({type:'join',username:username}))
-    
+    games[sessionName].playerId = 0
+    games[sessionName].players[playerId] = data.username
+
+    # Start listening on Redis if this is your first game
     if thread == None:
         thread = sessions.run_in_thread(sleep_time=0.001)
+    
+    # Tell other players in the game that you joined
+    Redis.public(sessionName, json.dumps({type:'join',username:username}))
 
 def handleSession(message):
-    stdout.flush()
     if message == None:
         return
     if message['type'] != 'message':
@@ -46,18 +52,55 @@ def handleGame(message, session):
     data = json.loads(message)
     game = games[session]
 
+    # send your name and player id when you join, and store their username
     if data.type == 'join':
-        playerId = len(game.players)
-        game.players[playerId] = data.username
-    if data.type == 'player':
-        game.players[data.id] = data.username
-    if data.type == 'dealer':
+        for i in range(0,4):
+            if i not in game.players:
+                game.players[i] = data.username
+                break
+
+        Redis.publish(session, json.dumps({
+            id:game.playerId,
+            username:username
+        }))
+
+    # Tell new players that you're in the game
+    elif data.type == 'player':
+        # Check if there's already a play with my id
+        if data.id == game.playerId and data.username != username:
+            while game.playerId in game.players:
+                game.playerId = game.playerId + 1
+
+        if not data.id in game.players:
+            game.players[data.id] = data.username
+
+    elif data.type == 'dealer' and data.player == game.playerId:
         game.dealing = true
-    if data.type == 'trump':
+        game.deck = shuffleDeck()
+        dealCards()
+
+    elif data.type == 'suit':
         game.trump = data.suit
-    if data.type == 'dealtCard':
+
+    elif data.type == 'deal' and data.player == game.playerId:
         game.hand.append(data.card)
-    if data.type == 'playCard':
+        sendMessage({type:'deal', card:data.card})
+
+    elif data.type == 'topCard':
+        game.topCard = data.card
+        sendMessage({type:'topCard', card:data.card})
+
+    elif data.type == 'turn':
+        sendMessage({type:'turn'})
+
+    elif data.type == 'pickItUp' and game.dealing:
+        sendMessage({type:'pickItUp', card:data.card})
+        Redis.publish(session, json.dumps({
+            type:'suit',
+            suit:data.card.suit
+        }))
+
+    elif data.type == 'playCard':
         game.gameCards.append(data.card)
         sendMessage({type:'playCard', user:data.username, card:data.card})
         
